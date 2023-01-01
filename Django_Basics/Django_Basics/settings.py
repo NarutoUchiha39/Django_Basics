ca3d07e64c1560007b2e709f99ccce037997a916
NarutoUchiha39 <anirudhata123@gmail.com>
2023-01-01 20:01:52 +0530

    Final

--- Django_Basics/Django_Basics/settings.py
+++ Django_Basics/Django_Basics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i$r)jeyv(jptmna0c71bk7u^b^mm03op5!7zwov2r97!8+(m!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'Django_Basics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Django',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '9820042597',
     }
-}
+} 
 
 
 # Password validation
--- Project/models.py
+++ Project/models.py
@@ -1,3 +1,2 @@
 from django.db import models
-
-# Create your models here.
+class users(models.Model):
